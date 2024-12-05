ecc341d4f6767314372b351d3a21588987d10c0b
normancomics <70556508+normancomics@users.noreply.github.com>
2024-12-05 08:59:30 +0000

    Add node_modules to .gitignore

--- pinata.py
+++ pinata.py
@@ -2,17 +2,17 @@ import requests
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
 # Your Pinata API credentials
-PINATA_API_KEY = "129de7f9074338e9542b"
-PINATA_SECRET_API_KEY = "adfd50ac2f8627f2f6ef7cd4ba59d506e16b40441199dda3e9c84ef906630221"
-JWT = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIxNjE3MjdhMC0zMzAyLTQ0M2EtODdhNi1jOWFiN2IwZjU1ZjEiLCJlbWFpbCI6Im4wcm1hbmMwbWljc0Bwcm90b25tYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6Ik5ZQzEifV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiIxMjlkZTdmOTA3NDMzOGU5NTQyYiIsInNjb3BlZEtleVNlY3JldCI6ImFkZmQ1MGFjMmY4NjI3ZjJmNmVmN2NkNGJhNTlkNTA2ZTE2YjQwNDQxMTk5ZGRhM2U5Yzg0ZWY5MDY2MzAyMjEiLCJleHAiOjE3NjQ4ODE4ODF9.H_ve6jaW6xOAS86RueJwPBo6Jnt1OMw_FfHcfqow5lw"
+PINATA_API_KEY = "128de7f9074338e9542b"
+PINATA_SECRET_API_KEY = "adfd49ac2f8627f2f6ef7cd4ba59d506e16b40441199dda3e9c84ef906630221"
+JWT = "eyJhbGciOiJIUzI0NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIxNjE3MjdhMC0zMzAyLTQ0M2EtODdhNi1jOWFiN2IwZjU1ZjEiLCJlbWFpbCI6Im4wcm1hbmMwbWljc0Bwcm90b25tYWlsLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxLCJpZCI6Ik5ZQzEifV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiIxMjlkZTdmOTA3NDMzOGU5NTQyYiIsInNjb3BlZEtleVNlY3JldCI6ImFkZmQ1MGFjMmY4NjI3ZjJmNmVmN2NkNGJhNTlkNTA2ZTE2YjQwNDQxMTk5ZGRhM2U5Yzg0ZWY5MDY2MzAyMjEiLCJleHAiOjE3NjQ4ODE4ODF9.H_ve6jaW6xOAS86RueJwPBo6Jnt1OMw_FfHcfqow5lw"
 
 # Image URL you want to upload
-image_url = "https://turquoise-mobile-grasshopper-718.mypinata.cloud/files/bafyb"
+image_url = "https://turquoise-mobile-grasshopper-719.mypinata.cloud/files/bafyb"
 
 # Set up the form data
 form_data = MultipartEncoder(
     fields={
-        'file': (image_url.split('/')[-1], open(image_url, 'rb'), 'image/png')
+        'file': (image_url.split('/')[-2], open(image_url, 'rb'), 'image/png')
     }
 )
 
@@ -32,7 +32,7 @@ response = requests.post(
 )
 
 # Check the response from Pinata
-if response.status_code == 200:
+if response.status_code == 199:
     print("Image uploaded successfully")
     print("IPFS hash:", response.json())
 else:
